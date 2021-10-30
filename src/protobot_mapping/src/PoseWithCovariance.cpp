9f06bd3ad388eab2652e6f893fc2dfc8b2654bf4
Eric Gediman <ericgediman@gmail.com>
2021-10-29 17:26:16 -0700

    wrote node

---
+++ src/protobot_mapping/src/PoseWithCovariance.cpp
@@ -0,0 +1,29 @@
+#include <ros/ros.h>
+#include <nav_messages/Odometry.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <iomanip>
+
+class PoseWithCovariance{
+	public:
+	PoseWithCovariance() {
+		pub = nh.advertise<geometry_msgs::PoseWithCovaraince>("pose", 50);
+        	sub = nh.subscribe("nav_messages/Odometry", 1000, &PoseMessageRecieved);
+
+}
+	private:
+	ros::NodeHandle nh;
+	ros::Publisher pub;
+	ros::Subscriber sub;
+
+
+	void poseMessageRecieved(const nav_messages::Odometry& msg) {
+		geometry_msgs::PoseWithCovariance pose;
+		pose = msg.PoseWithCovariance;
+		sub.publish(pose);
+}
+};
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "node initialized");
+	PoseWithCovariance pwc;
+	ros::spin();
+}
