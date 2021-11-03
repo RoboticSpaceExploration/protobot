19672895bc58cb26a6f265be584504a51bc4109f
Eric G <36492987+EricVin8@users.noreply.github.com>
2021-11-02 19:31:01 -0700

    Add files via upload

---
+++ src/protobot_mapping/src/OdometryTransform.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <iomanip>
+
+class OdometryTransform{
+	public:
+	OdometryTransform() {
+		pub = nh.advertise<geometry_msgs::PoseWithCovariance>("pose", 50);
+        	sub = nh.subscribe("nav_msgs/Odometry", 1000, &OdometryTransform::poseMessageRecieved, this);
+}
+	private:
+	ros::NodeHandle nh;
+	ros::Publisher pub;
+	ros::Subscriber sub;
+
+
+	void poseMessageRecieved(const nav_msgs::Odometry& msg) {
+		geometry_msgs::PoseWithCovariance pose2;
+		pose2 = msg.pose;
+		pub.publish(pose2);
+}
+};
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "node initialized");
+	OdometryTransform pwc;
+	ros::spin();
+}
