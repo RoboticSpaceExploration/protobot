88fae5884cb5ee75f5f3431ebeaf4361e5cdda62
Bret Witt <legonxtboy@gmail.com>
2022-02-07 19:48:52 -1000

    Fix build error (#77)

--- src/protobot_description/src/OdomToTF.cpp
+++ src/protobot_description/src/OdomToTF.cpp
@@ -5,10 +5,11 @@
 void poseCallback(const nav_msgs::Odometry& msg){
   static tf::TransformBroadcaster br;
   tf::Transform transform;
-  tf::Transformer transformer;
+  //tf::Transformer transformer;
+
   geometry_msgs::Pose pose = msg.pose.pose;
   
-  transformer.transformPose("base_link", pose, pose);
+  //transformer.transformPose("base_link", pose, pose);
 
   transform.setOrigin( tf::Vector3(pose.position.x, pose.position.y, pose.position.z) );
   tf::Quaternion q(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w);
