6bfd0877f36685805fc41e513fb96f38183f86fe
Jacob Sequeira <jacobseq@hawaii.edu>
2022-02-17 11:11:38 -1000

    -cleaned up OdometryTransform.cpp & OdomToTF.cpp as per cpplint -added & revised licenses

--- src/protobot_control/include/roboclaw.h
+++ src/protobot_control/include/roboclaw.h
@@ -1,6 +1,6 @@
 /* MIT License
 
-Copyright (c) [2022] [Jacob Anthony Sequeira]
+Copyright (c) [2022] [VIP Team RoSE]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/protobot_mapping/src/OdometryTransform.cpp
+++ src/protobot_mapping/src/OdometryTransform.cpp
@@ -1,30 +1,54 @@
+/* MIT License
+
+Copyright (c) [2022] [VIP Team RoSE]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <iomanip>
 
 class OdometryTransform{
-	public:
-	OdometryTransform() {
-		pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("pose", 50);
-        	sub = nh.subscribe("nav_msgs/Odometry", 1000, &OdometryTransform::poseMessageRecieved, this);
+ public:
+    OdometryTransform() {
+        pub = nh.advertise<geometry_msgs
+            ::PoseWithCovarianceStamped>("pose", 50);
+        sub = nh.subscribe("nav_msgs/Odometry",
+                           1000,
+                           &OdometryTransform::poseMessageRecieved, this);
 }
-	private:
-	ros::NodeHandle nh;
-	ros::Publisher pub;
-	ros::Subscriber sub;
+ private:
+    ros::NodeHandle nh;
+    ros::Publisher pub;
+    ros::Subscriber sub;
 
 
-	void poseMessageRecieved(const nav_msgs::Odometry& msg) {
-		geometry_msgs::PoseWithCovarianceStamped pose2;
-		pose2.header = msg.header;
-		pose2.pose = msg.pose;
-		pub.publish(pose2);
-	}
+    void poseMessageRecieved(const nav_msgs::Odometry& msg) {
+        geometry_msgs::PoseWithCovarianceStamped pose2;
+        pose2.header = msg.header;
+        pose2.pose = msg.pose;
+        pub.publish(pose2);
+    }
 };
 
 int main(int argc, char **argv) {
-	ros::init(argc, argv, "node initialized");
-	OdometryTransform pwc;
-	ros::spin();
+    ros::init(argc, argv, "node initialized");
+    OdometryTransform pwc;
+    ros::spin();
 }
