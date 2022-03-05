b5b8ab9d7803b968f9fc992a1cf0d76277c3283b
Winston Co <winstonco@live.com>
2022-03-04 18:57:44 -1000

    Update gps_goal.py

--- src/protobot_planner/nodes/gps_goal.py
+++ src/protobot_planner/nodes/gps_goal.py
@@ -2,9 +2,9 @@
 """
 GPS Goal Node
 """
+import math
 import rospy
 import click
-import math
 import actionlib
 import tf
 
@@ -98,8 +98,8 @@ def calc_goal(origin_lat, origin_long, goal_lat, goal_long):
     # by finding side lenghs of a right-angle triangle
     # Convert azimuth to radians
     azimuth = math.radians(azimuth)
-    x = adjacent = math.cos(azimuth) * hypotenuse
-    y = opposite = math.sin(azimuth) * hypotenuse
+    x = math.cos(azimuth) * hypotenuse # adjacent
+    y = math.sin(azimuth) * hypotenuse # opposite
     rospy.loginfo(f"The translation from the origin to the goal is (x,y) {x:.3f}, {y:.3f} m.")
 
     return x, y
@@ -107,8 +107,18 @@ def calc_goal(origin_lat, origin_long, goal_lat, goal_long):
 class GpsGoal():
     """
     This class generates a move_base goal from the rover's position
-    and a target goal pose.
+    to a target goal..
     """
+    # x, y, z, yaw, roll, pitch of the goal pose w.r.t. rover
+    goal_pose = {
+        "x" : 0,
+        "y" : 0,
+        "z" : 0,
+        "roll" : 0,
+        "pitch" : 0,
+        "yaw" : 0
+    }
+
     def __init__(self):
         """
         Intializes gps_goal node and connects itself to move_base.
@@ -126,29 +136,23 @@ class GpsGoal():
         # topic /local_xy_origin. We use this to calculate our goal within the map frame.
         self.origin_lat, self.origin_long = get_origin_lat_long()
     
-    def do_gps_goal(self, goal_lat, goal_long, z=0, yaw=0, roll=0, pitch=0):
+    def do_gps_goal(self, goal_lat, goal_long):
         """
-        Calculate goal x and y in the frame_id given
-        the frame's origin GPS and a goal GPS location.
+        Gets the x and y translation to the goal lat long and sets it to move_base to be published.
         Parameters
         ----------
         goal_lat
             The goal coordinate latitude
         goal_long
             The goal coordinate longitude
-        z : int
-            The z-position (elevation, default is 0)
-        yaw : int
-            The yaw (default is 0)
-        roll : int
-            The roll (default is 0)
-        pitch : int
-            The pitch (default is 0)
         """
+        #Calculates goal x and y in the frame_id given the frame's origin GPS and a goal GPS location.
         x, y = calc_goal(self.origin_lat, self.origin_long, goal_lat, goal_long)
+        self.goal_pose["x"] = x
+        self.goal_pose["y"] = y
 
         # Create move_base goal
-        self.publish_goal(x=x, y=y, z=z, yaw=yaw, roll=roll, pitch=pitch)
+        self.publish_goal()
 
     def gps_goal_pose_callback(self, data):
         """
@@ -160,12 +164,12 @@ class GpsGoal():
         """
         lat = data.pose.position.y
         long = data.pose.position.x
-        z = data.pose.position.z
+        self.goal_pose["z"] = data.pose.position.z
         euler = tf.transformations.euler_from_quaternion(data.pose.orientation)
-        roll = euler[0]
-        pitch = euler[1]
-        yaw = euler[2]
-        self.do_gps_goal(lat, long, z=z, yaw=yaw, roll=roll, pitch=pitch)
+        self.goal_pose["roll"] = euler[0]
+        self.goal_pose["pitch"] = euler[1]
+        self.goal_pose["yaw"] = euler[2]
+        self.do_gps_goal(lat, long)
 
     def gps_goal_fix_callback(self, data):
         """
@@ -177,37 +181,31 @@ class GpsGoal():
         """
         self.do_gps_goal(data.latitude, data.longitude)
 
-    def publish_goal(self, x=0, y=0, z=0, yaw=0, roll=0, pitch=0):
+    def publish_goal(self):
         """
         Creates a new move_base goal, sends the goal, and waits for goal result.
-        Parameters
-        ----------
-        x : int
-            The x position (default is 0)
-        y : int
-            The y position (default is 0)
-        z : int
-            The z position (elevation, default is 0)
-        yaw : int
-            The yaw (default is 0)
-        roll : int
-            The roll (default is 0)
-        pitch : int
-            The pitch (default is 0)
         """
+        x = self.goal_pose["x"]
+        y = self.goal_pose["y"]
+        z = self.goal_pose["z"]
+        roll = self.goal_pose["roll"]
+        pitch = self.goal_pose["pitch"]
+        yaw = self.goal_pose["yaw"]
+
         # Create move_base goal
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = rospy.get_param('~frame_id','map')
         goal.target_pose.pose.position.x = x
         goal.target_pose.pose.position.y = y
-        goal.target_pose.pose.position.z =  z
+        goal.target_pose.pose.position.z = z
         quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
         goal.target_pose.pose.orientation.x = quaternion[0]
         goal.target_pose.pose.orientation.y = quaternion[1]
         goal.target_pose.pose.orientation.z = quaternion[2]
         goal.target_pose.pose.orientation.w = quaternion[3]
         rospy.loginfo(f'Executing move_base goal to position (x,y) {x}, {y}, with {yaw} degrees yaw.')
-        rospy.loginfo("To cancel the goal: 'rostopic pub -1 /move_base/cancel actionlib_msgs/GoalID -- {}'")
+        rospy.loginfo(
+            "To cancel the goal: 'rostopic pub -1 /move_base/cancel actionlib_msgs/GoalID -- {}'")
 
         # Send goal
         self.move_base.send_goal(goal)
@@ -229,7 +227,7 @@ class GpsGoal():
 @click.option('--roll', '-r', help='Set target roll for goal', default=0.0)
 @click.option('--pitch', '-p', help='Set target pitch for goal', default=0.0)
 @click.option('--yaw', '-y', help='Set target yaw for goal', default=0.0)
-def cli_main(lat, long, roll, pitch, yaw):
+def cli_main(lat, long):
     """
     Send a goal to move_base given latitude and longitude.
     Notes
@@ -245,22 +243,17 @@ def cli_main(lat, long, roll, pitch, yaw):
         The goal coordinate latitude
     long
         The goal coordinate longitude
-    roll
-        The roll
-    pitch
-        The pitch
-    yaw
-        The yaw
     """
     gps_goal = GpsGoal()
 
     # Check for degrees, minutes, seconds format and convert to decimal
     lat, long = dms_to_decimal_format(lat, long)
-    gps_goal.do_gps_goal(lat, long, roll=roll, pitch=pitch, yaw=yaw)
+    gps_goal.do_gps_goal(lat, long)
 
 def ros_main():
-    gpsGoal = GpsGoal()
+    #gpsGoal = GpsGoal()
     rospy.spin()
 
 if __name__ == '__main__':
-    cli_main()
+    lat, long = get_origin_lat_long()
+    cli_main(lat, long)
