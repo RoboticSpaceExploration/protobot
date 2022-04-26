4b9099915de3ba26212dc461a0f68a2b82496950
Winston Co <winstonco@live.com>
2022-04-25 14:27:17 -1000

    nav_to_goal outline created

--- src/protobot_behavior/src/states/nav_to_goal_state.py
+++ src/protobot_behavior/src/states/nav_to_goal_state.py
@@ -7,9 +7,12 @@ import smach_ros
 
 class NavToGoal(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=["reached_goal", "aborted_goal"])
+        smach.State.__init__(self, outcomes=["reached_goal", "aborted_goal", "start_search"],
+                                   input_keys=["coord"])
 
     def execute(self, userdata):
-        rospy.loginfo("Executing state NAME")
+        rospy.loginfo("Executing state NAV_TO_GOAL")
+        userdata.coord
         return "reached_goal"
         return "aborted_goal"
+        return "start_search"
